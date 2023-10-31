3cde053a42c19530856782e6d3dca80b9787c124
supri <supri@vorta.live>
2023-10-31 13:50:49 +0700

    update

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -4,9 +4,16 @@
         <!-- Menu -->
 
         <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+        <div class="text-center">
+        <img src="{{url('/assets/img/logobulat.png')}}"  width="90"/>
+        </div>
+
           <div class="app-brand demo">
+
             <a href="{{route('dashboard')}}" class="app-brand-link">
+            <div class="text-center">
               <b>PT.Karya Indah Trasindo</b>
+            </div>
             </a>
 
             <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
